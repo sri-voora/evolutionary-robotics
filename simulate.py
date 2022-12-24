68854c0936065ec1f874f2d45d0defcad53c78a0
sri-voora <voora.srinitya@gmail.com>
2022-12-23 19:52:53 -0600

    Changing for loop from list.

--- simulate.py
+++ simulate.py
@@ -1,7 +1,7 @@
 import pybullet as p
 import time
 physicsClient = p.connect(p.GUI)
-for i in list(range(1,1001,1)):
+for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
     print(i)
