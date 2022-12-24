c498c750193b2bb8a378460c37161f45cb639437
sri-voora <voora.srinitya@gmail.com>
2022-12-23 21:19:26 -0600

    Building a tower.

--- generate.py
+++ generate.py
@@ -4,15 +4,11 @@ x=0
 y=0
 z=0.5
 
-x2=1
-y2=0
-z2=1.5
-
 length=1
 width=1
 height=1
 
 pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2], size=[length,width,height])
+for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[x,y,(z+i)], size=[length,width,height])
 pyrosim.End()
