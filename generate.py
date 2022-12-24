83a771e1c2dc6e1198076ccbf6bbefd38c42817e
sri-voora <voora.srinitya@gmail.com>
2022-12-23 21:36:21 -0600

    Making many links with for loops.

--- generate.py
+++ generate.py
@@ -9,6 +9,8 @@ width=1
 height=1
 
 pyrosim.Start_SDF("boxes.sdf")
-for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[x,y,(z+i)], size=[length,width,height])
+for c in range(5):
+    for r in range(5):
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[(x+c),(y+r),(z+i)], size=[(length*(0.9**i)),(width*(0.9**i)),(height*(0.9**i))])
 pyrosim.End()
